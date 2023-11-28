2c404d9e571efa559e85f61fdf1cd3d5170df0bd
georgparis <92294929+georgparis@users.noreply.github.com>
2023-11-28 22:54:04 +0100

    lets try this

---
+++ app/sprueche.tsx
@@ -0,0 +1,55 @@
+export const sprueche = (tuerchen: any) => {
+  switch (tuerchen) {
+    case "01-12-2023":
+      return "Junge die 90er haben angerufen, sie wollen ihre Website zurück";
+    case "02-12-2023":
+      return "Wie nennt man einen Elch mit Holzbein? Holzbein Elch!";
+    case "03-12-2023":
+      return "Warum war der Weihnachtsbaum im Fernsehen? Weil er geschmückt war!";
+    case "04-12-2023":
+      return "Wie nennt man einen pingeligen Weihnachtsmann? Knickriglaus!";
+    case "05-12-2023":
+      return "Warum war der Adventskalender genervt? Er hatte zu viele Tage!";
+    case "06-12-2023":
+      return "Ich fall vom Bett!!! Schon ist es Nikolaus!";
+    case "07-12-2023":
+      return "Junge die Spermie hat angerufen, sie will ihr Gehirn zurück";
+    case "08-12-2023":
+      return "Was sagt ein Schneemann zum anderen? 'Ist mir alles so eine Frost!'";
+    case "09-12-2023":
+      return "Einfach NRW verbot erteilen! Krass wie die sich benehmen!";
+    case "10-12-2023":
+      return "Was wünscht sich ein Schneemann zum Geburtstag? Eine Aufwärm-Party!";
+    case "11-12-2023":
+      return "Warum hat der Weihnachtsmann einen Garten? KP, aber er hat einen!";
+    case "12-12-2023":
+      return "Wie nennt man einen Weihnachtsmann, der in der Lotterie gewonnen hat? Glücksmann!   (So wie ich mit dir)";
+    case "13-12-2023":
+      return "Acab oder so, kp ich bin nicht so politisch";
+    case "14-12-2023":
+      return "Ein geiler Arsch findet auch mal ein Centstück!";
+    case "15-12-2023":
+      return "Warum nicht mal ein Couple Bild?";
+    case "16-12-2023":
+      return "Du bist so schön pup ild sehr";
+    case "17-12-2023":
+      return "Sollen wir da wieder hin?";
+    case "18-12-2023":
+      return "Junge war das ein Act aufs Schloss zu laufen!";
+    case "19-12-2023":
+      return "Was muss das muss nech?";
+    case "20-12-2023":
+      return "Junge wer ist das?";
+    case "21-12-2023":
+      return "Was zur Hölle macht Rudi hier?";
+    case "22-12-2023":
+      return "Wann Urli?";
+    case "23-12-2023":
+      return "Noch ein mal Schlafen!";
+    case "24-12-2023":
+      return "Waaas?! Georg hat doch ein Geschenk?!";
+
+    default:
+      return "24-12-2023";
+  }
+};
